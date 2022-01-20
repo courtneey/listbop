8c3f18d90c7a11fed208dd3c2142063a6e867554
courtney <rebocourtney@gmail.com>
2022-01-20 14:43:07 -0800

    created MUI grid form elements

--- src/components/SignupForm.tsx
+++ src/components/SignupForm.tsx
@@ -3,6 +3,9 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import LoginToggle from "./LoginToggle";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 export default function SignupForm() {
   return (
@@ -18,7 +21,41 @@ export default function SignupForm() {
       >
         <CardContent>
           <LoginToggle />
-          <Typography>Form will go here</Typography>
+          <Box
+            sx={{
+              flexGrow: 1,
+              marginTop: 2,
+            }}
+          >
+            <Grid container spacing={3} p={3}>
+              <Grid item xs={6}>
+                <TextField
+                  id="firstname"
+                  label="First Name"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField id="lastname" label="Last Name" variant="standard" />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="email"
+                  label="Email"
+                  variant="standard"
+                  sx={{ width: "100%" }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="password"
+                  label="Password"
+                  variant="standard"
+                  sx={{ width: "100%" }}
+                />
+              </Grid>
+            </Grid>
+          </Box>
         </CardContent>
       </Card>
     </div>

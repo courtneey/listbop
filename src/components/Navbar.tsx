174fc45e074e6477672e7f8068078b2240eb6c8f
courtney <rebocourtney@gmail.com>
2022-01-21 12:33:42 -0800

    created placeholder for navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="navbar-logo">
+        <img src={require("../logo.png")} alt="logo" style={{ width: 80 }} />
+        <img
+          src={require("../text-logo.png")}
+          alt="logo"
+          style={{
+            width: "90px",
+            height: "30px",
+            alignSelf: "center",
+            marginLeft: 20,
+          }}
+        />
+      </div>
+      <div className="navbar-links">
+        <Typography sx={{ fontFamily: "Roboto" }} color="primary.light">
+          My Lists
+        </Typography>
+        <Typography sx={{ fontFamily: "Roboto" }} color="primary.light">
+          New List
+        </Typography>
+        <Typography sx={{ fontFamily: "Roboto" }} color="primary.light">
+          Account
+        </Typography>
+      </div>
+    </div>
+  );
+}
--- src/components/NewListPage.tsx
+++ src/components/NewListPage.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NewListSimpleForm from "./NewListSimpleForm";
 import NewListEditForm from "./NewListEditForm";
+import Navbar from "./Navbar";
 
 export default function NewListPage() {
   return (
-    <div className="newlist-container">
-      <NewListSimpleForm />
-      <NewListEditForm />
+    <div>
+      <Navbar />
+      <div className="newlist-container">
+        <NewListSimpleForm />
+        <NewListEditForm />
+      </div>
     </div>
   );
 }

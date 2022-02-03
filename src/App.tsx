b117334507da1f62aba7508f22ced92995a1cd33
courtney <rebocourtney@gmail.com>
2022-02-03 12:20:24 -0800

    Revert "created secret module"
    This reverts commit f243841343edb8076223d6b0b4c74e5ca4ed1ee6.

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <LoginSignupPage />
-        {/* <NewListPage /> */}
+        {/* <LoginSignupPage /> */}
+        <NewListPage />
       </div>
     </ThemeProvider>
   );

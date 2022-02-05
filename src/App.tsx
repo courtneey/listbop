6396a45376d7398258a4d3025c4817ba34cd49ea
courtney <rebocourtney@gmail.com>
2022-02-04 17:35:37 -0800

    user can log in and is routed to new list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { LoginSignupPage } from "./client/components/LoginSignupPage";
 import NewListPage from "./client/components/NewListPage";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { connect } from "react-redux";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { me } from "./client/store/auth";
+import { RootState, AppDispatch } from "./client/store";
 
 const theme = createTheme({
   palette: {
@@ -12,15 +16,47 @@ const theme = createTheme({
   },
 });
 
-function App() {
+interface AppProps {
+  loggedIn: boolean;
+  loadInitialData: () => void;
+}
+
+function App(props: AppProps) {
+  const { loggedIn, loadInitialData } = props;
+
+  useEffect(() => {
+    loadInitialData();
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <LoginSignupPage />
-        {/* <NewListPage /> */}
+        <BrowserRouter>
+          <Routes>
+            {loggedIn ? (
+              <Route path="/" element={<NewListPage />}></Route>
+            ) : (
+              <Route path="/" element={<LoginSignupPage />}></Route>
+            )}
+          </Routes>
+        </BrowserRouter>
       </div>
     </ThemeProvider>
   );
 }
 
-export default App;
+const mapState = (state: RootState) => {
+  return {
+    loggedIn: !!state.auth.id,
+  };
+};
+
+const mapDispatch = (dispatch: AppDispatch) => {
+  return {
+    loadInitialData() {
+      dispatch(me());
+    },
+  };
+};
+
+export default connect(mapState, mapDispatch)(App);

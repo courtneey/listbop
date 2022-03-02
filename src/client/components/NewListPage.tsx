d768198f5cf3d474969eeb4722bf0a1dd17ad09a
courtney <rebocourtney@gmail.com>
2022-03-02 13:43:37 -0800

    retrieve token from kroger web api using fetch api

--- src/client/components/NewListPage.tsx
+++ src/client/components/NewListPage.tsx
@@ -3,6 +3,9 @@ import NewListSimpleForm from "./NewListSimpleForm";
 import NewListEditForm from "./NewListEditForm";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import axios from "axios";
+import { API_ID, API_KEY, OAUTH2_BASE_URL, API_BASE_URL } from "../../secret";
+import { Buffer } from "buffer";
 
 interface Item {
   id: number;
@@ -16,8 +19,53 @@ export default function NewListPage() {
     { id: 2, name: "Bread", category: "Bakery" },
     { id: 3, name: "Milk", category: "Dairy" },
   ]);
+  const encoded = Buffer.from(`${API_ID}:${API_KEY}`);
+  const authorization = `Basic ${encoded.toString("base64")}`;
+
+  const fetchCategory = async (name: string) => {
+    let token = null;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Authorization", `${authorization}`);
+    const urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "client_credentials");
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: "follow",
+    };
+
+    await fetch(
+      "https://api-ce.kroger.com/v1/connect/oauth2/token",
+      requestOptions as any
+    )
+      .then((response) => response.text())
+      .then((result) => {
+        const { access_token } = JSON.parse(result);
+        token = access_token;
+      })
+      .catch((error) =>
+        console.log("There was an issue with fetching the token:", error)
+      );
+
+    // const res = await axios.get(
+    //   `https://api-ce.kroger.com/v1/products?filter.term=${name}&filter.limit=1`,
+    //   {
+    //     headers: {
+    //       Authorization: `${authorization}`,
+    //       "Content-Type": "application/json; charset=utf-8",
+    //     },
+    //   }
+    // );
+    // console.log("res:", res);
+  };
 
   const addToList = (item: Item) => {
+    fetchCategory(item.name);
+
     const newList = list.slice();
     newList.push(item);
     setList(newList);

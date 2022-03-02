b6df16f2a862e99cc3e16cf1b43eee2d489b0854
courtney <rebocourtney@gmail.com>
2022-03-02 15:20:54 -0800

    retrieve first category returned from kroger api

--- src/client/components/NewListPage.tsx
+++ src/client/components/NewListPage.tsx
@@ -4,8 +4,7 @@ import NewListEditForm from "./NewListEditForm";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import axios from "axios";
-import { API_ID, API_KEY, OAUTH2_BASE_URL, API_BASE_URL } from "../../secret";
-import { Buffer } from "buffer";
+import { API_BASE_URL } from "../../secret";
 
 interface Item {
   id: number;
@@ -13,54 +12,31 @@ interface Item {
   category: string;
 }
 
-export default function NewListPage() {
+interface Props {
+  krogerToken: string;
+}
+
+export default function NewListPage(props: Props) {
   const [list, setList] = useState<Item[] | []>([
     { id: 1, name: "Cookies", category: "Snacks" },
     { id: 2, name: "Bread", category: "Bakery" },
     { id: 3, name: "Milk", category: "Dairy" },
   ]);
-  const encoded = Buffer.from(`${API_ID}:${API_KEY}`);
-  const authorization = `Basic ${encoded.toString("base64")}`;
+  const { krogerToken: token } = props;
 
   const fetchCategory = async (name: string) => {
-    let token = null;
-
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-    myHeaders.append("Authorization", `${authorization}`);
-    const urlencoded = new URLSearchParams();
-    urlencoded.append("grant_type", "client_credentials");
-
-    const requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: "follow",
-    };
-
-    await fetch(
-      "https://api-ce.kroger.com/v1/connect/oauth2/token",
-      requestOptions as any
-    )
-      .then((response) => response.text())
-      .then((result) => {
-        const { access_token } = JSON.parse(result);
-        token = access_token;
-      })
-      .catch((error) =>
-        console.log("There was an issue with fetching the token:", error)
-      );
-
-    // const res = await axios.get(
-    //   `https://api-ce.kroger.com/v1/products?filter.term=${name}&filter.limit=1`,
-    //   {
-    //     headers: {
-    //       Authorization: `${authorization}`,
-    //       "Content-Type": "application/json; charset=utf-8",
-    //     },
-    //   }
-    // );
-    // console.log("res:", res);
+    const { data } = await axios.get(`${API_BASE_URL}/v1/products`, {
+      params: {
+        "filter.term": `${name.toLowerCase()}`,
+        "filter.limit": 1,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Cache-Control": "no-cache",
+      },
+    });
+    const productInfo = data.data[0];
+    const category = productInfo.categories[0];
   };
 
   const addToList = (item: Item) => {

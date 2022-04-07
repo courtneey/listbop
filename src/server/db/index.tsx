713590a2b26d47dc8380fd448d1518d9e2667836
courtney <rebocourtney@gmail.com>
2022-04-07 11:36:15 -0700

    created one to many association between user and list

--- src/server/db/index.tsx
+++ src/server/db/index.tsx
@@ -2,8 +2,13 @@ export const db = require("./db");
 
 const User = require("./models/User");
 const Product = require("./models/Product");
+const List = require("./models/List");
+
+User.hasMany(List);
+List.belongsTo(User);
 
 export const models = {
   User,
   Product,
+  List,
 };
---
+++ src/server/db/models/List.js
@@ -0,0 +1,5 @@
+const db = require("../db");
+
+const List = db.define("list", {});
+
+module.exports = List;
